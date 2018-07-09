3f9982cc9aef8971a45d1d20cf137ddf350483ef
Inyong Hwang <inyong1020@gmail.com>
2018-07-09 14:35:40 +0900

    Add files via upload

---
+++ DeepLearning/Python/Chapter 1/Ch01-01-interpreter.py
@@ -0,0 +1,64 @@
+# terminal
+# > python --version
+# > python
+# >>> 1 + 2
+# >>> 1 - 2
+# >>> 4 * 5
+# >>> 7 / 5
+# >>> 3 ** 2
+# >>> type(10)
+# >>> type(2.718)
+# >>> type("hello")
+# >>> x = 10
+# >>> print(x)
+# >>> x = 100
+# >>> print(x)
+# >>> y = 3.14
+# >>> x * y
+# >>> type(x * y)
+# >>> a = [1, 2, 3, 4, 5]
+# >>> print(a)
+# >>> len(a)
+# >>> a[0]
+# >>> a[4]
+# >>> a[4] = 99
+# >>> print(a)
+# >>> print(a)
+# >>> a[0:2]
+# >>> a[1:]
+# >>> a[:3]
+# >>> a[:-1]
+# >>> a[:-2]
+# >>> me = {'height':180}
+# >>> me['height']
+# >>> me['weight'] = 70
+# >>> print(me)
+# >>> hungry = True
+# >>> sleepy = False
+# >>> type(hungry)
+# >>> not hungry
+# >>> hungry and sleepy
+# >>> hungry or sleepy
+# >>> hungry = Ture
+# >>> if hungry:
+# ...     print("I'm hungry")
+# ...
+# >>> hungry = False
+# >>> if hungry:
+# ...     print("I'm hungry")
+# ... else:
+# ...     print("I'm not hungry")
+# ...     print("I;m sleepy")
+# ...
+# >>> for i in [1, 2, 3]:
+# ...     print(i)
+# >>> def hello():
+# ...     print("Hello World!")
+# ...
+# >>> hello()
+# >>> def hello(object):
+# ...     print("Hello " + object + "!")
+# ...
+# >>> hello("cat")
+# Ctrl + D
+# Ctrl + Z and Enter
---
+++ DeepLearning/Python/Chapter 1/Ch01-04-numpy.py
@@ -0,0 +1,35 @@
+# >>> import numpy as np
+# >>> x = np.array([1.0, 2.0, 3.0])
+# >>> print(x)
+# >>> type(x)
+# >>> x = np.array([1.0, 2.0, 3.0])
+# >>> y = np.array([2.0, 4.0, 6.0])
+# >>> x + y
+# >>> x - y
+# >>> x / y
+# >>> x = np.array([1.0, 2.0, 3.0])
+# >>> x / 2.0
+# >>> A = np.array([[1, 2], [3, 4]])
+# >>> print(A)
+# >>> A.shape
+# >>> A.dtype
+# >>> B = np.array([[3, 0], [0, 6]])
+# >>> A + B
+# >>> A * B
+# >>> print(A)
+# >>> A * 10
+# >>> A = np.array([[1, 2], [3, 4]])
+# >>> B = np.array([10, 20])
+# >>> A * B
+# >>> X = np.array([[51, 55], [14, 19], [0, 4]])
+# >>> print(X)
+# >>> X[0]
+# >>> X[0][1]
+# >>> for row in X:
+# ...     print(row)
+# ...
+# >>> X = X.flatten()
+# >>> print(X)
+# >>> X[np.array([0, 2, 4])]
+# >>> X > 15
+# >>> X[X>15]
